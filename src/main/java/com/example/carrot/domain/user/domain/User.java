72b469157086d1df67c3a08832c067107c485227
kjhonggg95 <kjhonggg95@gmail.com>
2023-10-28 18:34:30 +0900

    feat: 회원 엔티티 생성

--- src/main/java/com/example/carrot/domain/user/domain/User.java
+++ src/main/java/com/example/carrot/domain/user/domain/User.java
@@ -1,11 +1,13 @@
 package com.example.carrot.domain.user.domain;
 
+import com.example.carrot.domain.user.dto.UserFormDto;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,11 +19,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
-    private String username;
-    private String password;
-    private String name;
-    private String nickname;
 
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nickname;
 
     @Builder
     public User(Long id, String username, String password, String name, String nickname) {
@@ -30,5 +39,15 @@ public class User {
         this.password = password;
         this.name = name;
         this.nickname = nickname;
+    }
+
+    public static User createUser(UserFormDto userFormDto, PasswordEncoder passwordEncoder) {
+        User user = User.builder()
+                .username(userFormDto.getUsername())
+                .password(passwordEncoder.encode(userFormDto.getPassword()))
+                .name(userFormDto.getName())
+                .nickname(userFormDto.getNickname())
+                .build();
+        return user;
     }
 }
